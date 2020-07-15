88e51534843c5636023fe45b3fb0ff6b42442cf9
Kidir Torain <kktorainnn@gmail.com>
2020-07-15 15:38:38 +0000

    Errors

---
+++ main.py
@@ -0,0 +1,12 @@
+example = "example"
+print("This is an", example, "and has no errors.")
+
+name = "Elia Deppe"
+FavoriteArtist= "Earl Sweatshirt"
+v = "Nosara"
+hobby = "Painting & Skateboarding"
+date = "06/20/22"
+
+print("My name is", name,"and my favorite artist is",FavoriteArtist)
+print("I love to go on vacation to", v, "and my two main hobbies are", hobby) 
+print("Today's date is:", date)
